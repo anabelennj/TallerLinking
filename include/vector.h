dcb5b84db2cc3be34a93ed434057b96cfe807ee3
anabelennj <anegrete@espol.edu.ec>
2017-11-14 20:46:18 -0500

    Nuevo tipo Vector

--- include/vector.h
+++ include/vector.h
@@ -1,3 +1,7 @@
+typedef struct Vector{
+	int vector[2];
+}Vector;
+
 
 void addvec(int *x, int *y, int *z, int n);
 void multvec(int *x, int *y, int *z, int n); 
--- src/main2.c
+++ src/main2.c
@@ -3,14 +3,25 @@
 #include <stdio.h>
 #include <vector.h>
 
-int x[2] = {1, 2};
-int y[2] = {3, 4};
+Vector vector1;
+Vector vector2;
+
+//int x[2] = {1, 2};
+//int y[2] = {3, 4}; 
+
 int z[2];
 
 int main() 
 {
-    addvec(x, y, z, 2);
-    printf("z = [%d %d]\n", z[0], z[1]);
-    return 0;
+	vector1.vector[0] = 1;
+	vector1.vector[1] = 2;
+	vector2.vector[0] = 3;
+	vector2.vector[1] = 4;
+
+	addvec(vector1.vector, vector2.vector, z, 2);
+	printf("z = [%d %d]\n", z[0], z[1]);
+	multvec(vector1.vector, vector2.vector, z, 2);
+	printf("z = [%d %d]\n", z[0], z[1]);
+	return 0;
 }
 /* $end main2 */
