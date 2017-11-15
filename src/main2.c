a84aad43a6975012834a0d8aad15c4f7b8e7ec89
anabelennj <anegrete@espol.edu.ec>
2017-11-15 02:18:47 -0500

    Valores por teclado

--- src/main2.c
+++ src/main2.c
@@ -4,22 +4,34 @@
 #include <vector.h>
 
 Vector vector1;
-Vector vector2;
-
-//int x[2] = {1, 2};
-//int y[2] = {3, 4}; 
+Vector vector2; 
 
 int z[2];
 
 int main() 
 {
-	vector1.vector[0] = 1;
-	vector1.vector[1] = 2;
-	vector2.vector[0] = 3;
-	vector2.vector[1] = 4;
+	int x, y;
+	int w, v;	
+	
+	printf("Ingrese el primer valor para el vector 1: ");
+	scanf("%d", &x);
+
+	printf("\nIngrese el segundo valor para el vector 1: ");
+	scanf("%d", &y);
+
+	printf("\n\nIngrese el primer valor para el vector 2: ");
+	scanf("%d", &w);
+
+	printf("\nIngrese el segundo valor para el vector 2: ");
+	scanf("%d", &v);	
+
+	vector1.vector[0] = x;
+	vector1.vector[1] = y;
+	vector2.vector[0] = w;
+	vector2.vector[1] = v;
 
 	addvec(vector1.vector, vector2.vector, z, 2);
-	printf("z = [%d %d]\n", z[0], z[1]);
+	printf("\nz = [%d %d]\n", z[0], z[1]);
 	multvec(vector1.vector, vector2.vector, z, 2);
 	printf("z = [%d %d]\n", z[0], z[1]);
 	return 0;
